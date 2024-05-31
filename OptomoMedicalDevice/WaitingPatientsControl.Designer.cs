e68bcd5133d42a1e41dbf6c678f7b2490f848448
Rasim Mutlu <rasim.mutlu99@gmail.com>
2024-05-31 13:52:53 +0300

    configure waitingPatientsList && projection form

--- OptomoMedicalDevice/WaitingPatientsControl.Designer.cs
+++ OptomoMedicalDevice/WaitingPatientsControl.Designer.cs
@@ -28,31 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("1, 12131, deneme");
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.label1 = new System.Windows.Forms.Label();
+            this.listWaitingPatients = new System.Windows.Forms.ListView();
             this.queueNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.patientID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.patientFullName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listView1
+            // listWaitingPatients
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listWaitingPatients.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.queueNumber,
             this.patientID,
             this.patientFullName});
-            this.listView1.FullRowSelect = true;
-            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.listView1.HideSelection = false;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(8, 38);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(438, 283);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listWaitingPatients.FullRowSelect = true;
+            this.listWaitingPatients.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listWaitingPatients.HideSelection = false;
+            this.listWaitingPatients.Location = new System.Drawing.Point(8, 38);
+            this.listWaitingPatients.Name = "listWaitingPatients";
+            this.listWaitingPatients.Size = new System.Drawing.Size(438, 283);
+            this.listWaitingPatients.TabIndex = 0;
+            this.listWaitingPatients.UseCompatibleStateImageBehavior = false;
+            this.listWaitingPatients.View = System.Windows.Forms.View.Details;
+            this.listWaitingPatients.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listWaitingPatients_MouseDoubleClick);
+            // 
+            // queueNumber
+            // 
+            this.queueNumber.Text = "Sira No";
+            // 
+            // patientID
+            // 
+            this.patientID.Text = "T.C. Kimlik No";
+            this.patientID.Width = 83;
+            // 
+            // patientFullName
+            // 
+            this.patientFullName.Text = "Ad - Soyad";
+            this.patientFullName.Width = 125;
             // 
             // label1
             // 
@@ -64,26 +76,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Bekleyen Hastalar";
             // 
-            // queueNumber
-            // 
-            this.queueNumber.Text = "Sira No";
-            // 
-            // patientID
-            // 
-            this.patientID.Text = "T.C. Kimlik";
-            // 
-            // patientFullName
-            // 
-            this.patientFullName.Text = "Ad - Soyad";
-            // 
             // WaitingPatientsControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listWaitingPatients);
             this.Name = "WaitingPatientsControl";
             this.Size = new System.Drawing.Size(539, 344);
+            this.Load += new System.EventHandler(this.WaitingPatientsControl_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,10 +92,10 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listWaitingPatients;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ColumnHeader queueNumber;
         private System.Windows.Forms.ColumnHeader patientID;
         private System.Windows.Forms.ColumnHeader patientFullName;
+        private System.Windows.Forms.ColumnHeader queueNumber;
     }
 }
